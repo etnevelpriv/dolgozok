aff384ab9d66b3b19fa7455e512e837edb0715e0
etnevelpriv <leventerastovits@gmail.com>
2026-03-27 14:22:14 +0100

    minimalis hibakezeles hozzaadasa

--- classes/EmployeeStatistics.ts
+++ classes/EmployeeStatistics.ts
@@ -9,22 +9,30 @@ class EmployeeStatistics {
         this.employee = Employee;
     };
 
-    getMaxSalary():number | undefined {
+    getMaxSalary(): number | undefined {
         let maxSalary: number | undefined = this.employee[0]?.salary;
-        this.employee.forEach((emp:Employee) => {
-            if (emp.salary > maxSalary) {
-                maxSalary = emp.salary;
-            };
-        });
-        return maxSalary;
+        if (typeof maxSalary != "number") {
+            throw new Error(`A 'getMaxSalary' nem tudott lefutni, a 'salary' valtozo tipusa: ${typeof maxSalary}`);
+        } else {
+            this.employee.forEach((emp: Employee) => {
+                if (emp.salary > maxSalary) {
+                    maxSalary = emp.salary;
+                };
+            });
+            return maxSalary;
+        };
     };
 
-    getAvarageAge():number | undefined {
-        let summedAges = 0;
-        this.employee.forEach((emp:Employee) => {
-            summedAges += emp.age;
-        });
-        const avgAge = (summedAges / (this.employee.length));
-        return avgAge;
+    getAvarageAge(): number | undefined {
+        let summedAges: number = 0;
+        if (typeof this.employee[0]?.age != "number") {
+            throw new Error(`A 'getAvarageAge' nem tudott lefutni, az 'age' valtozo tipusa: ${typeof this.employee[0]?.age}`);
+        } else {
+            this.employee.forEach((emp: Employee) => {
+                summedAges += emp.age;
+            });
+            const avgAge = (summedAges / (this.employee.length));
+            return avgAge;
+        };
     };
 };
