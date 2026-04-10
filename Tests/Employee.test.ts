1722af07722b7d200dd46798c90331be9cd9cf1b
etnevelpriv <leventerastovits@gmail.com>
2026-04-10 22:10:20 +0200

    2 adathalmaz letrehozva

--- Tests/Employee.test.ts
+++ Tests/Employee.test.ts
@@ -1,3 +1,38 @@
 import { describe, it, expect } from "vitest";
 import type { Employee } from "../classes/Employee.ts";
 import type { EmployeeStatistics } from "../classes/EmployeeStatistics.ts";
+
+const testEmployees_1: Employee[] = [
+    {
+        name: 'Levente',
+        age: 18,
+        salary: 3000
+    },
+    {
+        name: 'Roland',
+        age: 19,
+        salary: 5600
+    },
+    {
+        name: 'Jozsef',
+        age: 32,
+        salary: 2999
+    },
+];
+const testEmployees_2: Employee[] = [
+    {
+        name: 'Antal',
+        age: 90,
+        salary: 8900
+    },
+    {
+        name: 'Agoston',
+        age: 8,
+        salary: 500
+    },
+    {
+        name: 'Ferenc',
+        age: 54,
+        salary: 5454
+    },
+];
